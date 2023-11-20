be7f153bf4da18c5935d64bb028beffbf2a2cd4e
Haydara <haydaraal2005@gmail.com>
2023-11-20 10:19:28 +0100

    Added colors

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@ using Raylib_cs;
 
 internal class Program
 {
-    static void Main(string[] args)
+    // Colors.
+    static Color Green = Raylib.ColorFromNormalized(new System.Numerics.Vector4(0.467f, 0.6f, 0.329f, 1));
+    static Color White = Raylib.ColorFromNormalized(new System.Numerics.Vector4(0.914f, 0.929f, 0.8f, 1));
+    static Color Black = Color.BLACK;
+
+    static void Main()
     {
         Raylib.InitWindow(1080, 920, "Chess");
 
@@ -32,14 +37,14 @@ internal class Program
 
         // Drawing the outline of the board.
         // Using two lines to make the outline look thicker.
-        Raylib.DrawRectangleLines(starting_pos_x - 2, starting_pos_y - 2, 8 * width + 4, 8 * width + 4, Color.BLACK);
-        Raylib.DrawRectangleLines(starting_pos_x - 1, starting_pos_y - 1, 8 * width + 2, 8 * width + 2, Color.BLACK);
+        Raylib.DrawRectangleLines(starting_pos_x - 2, starting_pos_y - 2, 8 * width + 4, 8 * width + 4, Black);
+        Raylib.DrawRectangleLines(starting_pos_x - 1, starting_pos_y - 1, 8 * width + 2, 8 * width + 2, Black);
 
         // Drawing the board.
-        Raylib.DrawRectangle(starting_pos_x, starting_pos_y, width * 8, width * 8, Color.BEIGE);
+        Raylib.DrawRectangle(starting_pos_x, starting_pos_y, width * 8, width * 8, Green);
         for (int i = 0; i < 8; i++)
             for (int j = 0; j < 8; j++)
                 if ((i + j) % 2 == 0)
-                    Raylib.DrawRectangle(starting_pos_x + i * width, starting_pos_y + j * width, width, width, Color.LIME);
+                    Raylib.DrawRectangle(starting_pos_x + i * width, starting_pos_y + j * width, width, width, White);
     }
 }
