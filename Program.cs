9a1ef2ef9703433c16a5bd97aa098eb334dc8710
Haydara <haydaraal2005@gmail.com>
2023-11-22 13:50:33 +0100

    Cleaned code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿namespace Chess;
 
+// This file contains the UI and the logic to display the board.
+
 using Raylib_cs;
 using System.Numerics;
 using static Board;
@@ -10,6 +12,18 @@ internal class Program
     static Color Green = Raylib.ColorFromNormalized(new Vector4(0.467f, 0.600f, 0.329f, 1f));
     static Color White = Raylib.ColorFromNormalized(new Vector4(0.914f, 0.929f, 0.800f, 1f));
     static Color Black = Color.BLACK;
+
+    // Making the starting position as a variable to gain the ability to shift the board
+    static int starting_pos_x = 25;
+    static int starting_pos_y = 25;
+
+    // The width of the square
+    static int width = 100;
+    readonly static int ratio = width / 100;
+
+    // Repeated vairables
+    readonly static char on  = '1';
+    readonly static char off = '0';
 
     static Texture2D[] textures = new Texture2D[12];
 
@@ -53,13 +67,6 @@ internal class Program
 
     static void DrawChessBoard()
     {
-        // Making the starting position as a variable to gain the ability to shift the board
-        int starting_pos_x = 25;
-        int starting_pos_y = 25;
-
-        // The width of the square
-        int width = 100;
-
         // Drawing the outline of the board.
         // Using two lines to make the outline look thicker.
         Raylib.DrawRectangleLines(starting_pos_x - 2, starting_pos_y - 2, 8 * width + 4, 8 * width + 4, Black);
@@ -73,82 +80,112 @@ internal class Program
                     Raylib.DrawRectangle(starting_pos_x + i * width, starting_pos_y + j * width, width, width, White);
 
         // Drawing the ranks numbers.
-        Raylib.DrawText("1", starting_pos_x + (4 * width / 100), starting_pos_y + (4 * width / 100),             12 * width / 100, Green);
-        Raylib.DrawText("2", starting_pos_x + (4 * width / 100), starting_pos_y + (4 * width / 100) + width,     12 * width / 100, White);
-        Raylib.DrawText("3", starting_pos_x + (4 * width / 100), starting_pos_y + (4 * width / 100) + width * 2, 12 * width / 100, Green);
-        Raylib.DrawText("4", starting_pos_x + (4 * width / 100), starting_pos_y + (4 * width / 100) + width * 3, 12 * width / 100, White);
-        Raylib.DrawText("5", starting_pos_x + (4 * width / 100), starting_pos_y + (4 * width / 100) + width * 4, 12 * width / 100, Green);
-        Raylib.DrawText("6", starting_pos_x + (4 * width / 100), starting_pos_y + (4 * width / 100) + width * 5, 12 * width / 100, White);
-        Raylib.DrawText("7", starting_pos_x + (4 * width / 100), starting_pos_y + (4 * width / 100) + width * 6, 12 * width / 100, Green);
-        Raylib.DrawText("8", starting_pos_x + (4 * width / 100), starting_pos_y + (4 * width / 100) + width * 7, 12 * width / 100, White);
+        Raylib.DrawText("1", starting_pos_x + (4 * ratio), starting_pos_y + (4 * ratio),             12 * ratio, Green);
+        Raylib.DrawText("2", starting_pos_x + (4 * ratio), starting_pos_y + (4 * ratio) + width,     12 * ratio, White);
+        Raylib.DrawText("3", starting_pos_x + (4 * ratio), starting_pos_y + (4 * ratio) + width * 2, 12 * ratio, Green);
+        Raylib.DrawText("4", starting_pos_x + (4 * ratio), starting_pos_y + (4 * ratio) + width * 3, 12 * ratio, White);
+        Raylib.DrawText("5", starting_pos_x + (4 * ratio), starting_pos_y + (4 * ratio) + width * 4, 12 * ratio, Green);
+        Raylib.DrawText("6", starting_pos_x + (4 * ratio), starting_pos_y + (4 * ratio) + width * 5, 12 * ratio, White);
+        Raylib.DrawText("7", starting_pos_x + (4 * ratio), starting_pos_y + (4 * ratio) + width * 6, 12 * ratio, Green);
+        Raylib.DrawText("8", starting_pos_x + (4 * ratio), starting_pos_y + (4 * ratio) + width * 7, 12 * ratio, White);
 
         // Drawing the files letters.
-        Raylib.DrawText("a", starting_pos_x + (80 * width / 100),             starting_pos_y + width * 7 + (85 * width / 100), 12 * width / 100, White);
-        Raylib.DrawText("b", starting_pos_x + (80 * width / 100) + width ,    starting_pos_y + width * 7 + (85 * width / 100), 12 * width / 100, Green);
-        Raylib.DrawText("c", starting_pos_x + (80 * width / 100) + width * 2, starting_pos_y + width * 7 + (85 * width / 100), 12 * width / 100, White);
-        Raylib.DrawText("d", starting_pos_x + (80 * width / 100) + width * 3, starting_pos_y + width * 7 + (85 * width / 100), 12 * width / 100, Green);
-        Raylib.DrawText("e", starting_pos_x + (80 * width / 100) + width * 4, starting_pos_y + width * 7 + (85 * width / 100), 12 * width / 100, White);
-        Raylib.DrawText("f", starting_pos_x + (80 * width / 100) + width * 5, starting_pos_y + width * 7 + (85 * width / 100), 12 * width / 100, Green);
-        Raylib.DrawText("g", starting_pos_x + (80 * width / 100) + width * 6, starting_pos_y + width * 7 + (85 * width / 100), 12 * width / 100, White);
-        Raylib.DrawText("h", starting_pos_x + (80 * width / 100) + width * 7, starting_pos_y + width * 7 + (85 * width / 100), 12 * width / 100, Green);
+        Raylib.DrawText("a", starting_pos_x + (80 * ratio),             starting_pos_y + width * 7 + (85 * ratio), 12 * ratio, White);
+        Raylib.DrawText("b", starting_pos_x + (80 * ratio) + width ,    starting_pos_y + width * 7 + (85 * ratio), 12 * ratio, Green);
+        Raylib.DrawText("c", starting_pos_x + (80 * ratio) + width * 2, starting_pos_y + width * 7 + (85 * ratio), 12 * ratio, White);
+        Raylib.DrawText("d", starting_pos_x + (80 * ratio) + width * 3, starting_pos_y + width * 7 + (85 * ratio), 12 * ratio, Green);
+        Raylib.DrawText("e", starting_pos_x + (80 * ratio) + width * 4, starting_pos_y + width * 7 + (85 * ratio), 12 * ratio, White);
+        Raylib.DrawText("f", starting_pos_x + (80 * ratio) + width * 5, starting_pos_y + width * 7 + (85 * ratio), 12 * ratio, Green);
+        Raylib.DrawText("g", starting_pos_x + (80 * ratio) + width * 6, starting_pos_y + width * 7 + (85 * ratio), 12 * ratio, White);
+        Raylib.DrawText("h", starting_pos_x + (80 * ratio) + width * 7, starting_pos_y + width * 7 + (85 * ratio), 12 * ratio, Green);
     }
 
     static void DrawPieces()
     {
         UInt64[] pieces = Board.GetPiecesBB();
 
-        //Get pieces bitboards
-        string WPawns   = Convert.ToString((long)pieces[Board.White + Board.Pawns - 1], toBase: 2).PadLeft(64, '0'); 
-        string BPawns   = Convert.ToString((long)pieces[Board.Black + Board.Pawns - 1], toBase: 2).PadLeft(64, '0');
+        // Repeated variables
+        int bits = 64;
 
-        string WKnight  = Convert.ToString((long)pieces[Board.White + Board.Knights - 1], toBase: 2).PadLeft(64, '0');
-        string BKnight  = Convert.ToString((long)pieces[Board.Black + Board.Knights - 1], toBase: 2).PadLeft(64, '0');
-                                          
-        string WBishops = Convert.ToString((long)pieces[Board.White + Board.Bishops - 1], toBase: 2).PadLeft(64, '0');
-        string BBishops = Convert.ToString((long)pieces[Board.Black + Board.Bishops - 1], toBase: 2).PadLeft(64, '0');
-                                                                                   
-        string WRooks   = Convert.ToString((long)pieces[Board.White + Board.Rooks - 1],   toBase: 2).PadLeft(64, '0');
-        string BRooks   = Convert.ToString((long)pieces[Board.Black + Board.Rooks - 1],   toBase: 2).PadLeft(64, '0');
-                                          
-        string WQueen   = Convert.ToString((long)pieces[Board.White + Board.Queen - 1],   toBase: 2).PadLeft(64, '0');
-        string BQueen   = Convert.ToString((long)pieces[Board.Black + Board.Queen - 1],   toBase: 2).PadLeft(64, '0');
-                                          
-        string WKing    = Convert.ToString((long)pieces[Board.White + Board.King - 1],    toBase: 2).PadLeft(64, '0');
-        string BKing    = Convert.ToString((long)pieces[Board.Black + Board.King - 1],    toBase: 2).PadLeft(64, '0');
+        //Get pieces bitboards
+        string WPawns   = Convert.ToString((long)pieces[Board.White + Board.Pawns - 1],   toBase: 2).PadLeft(bits, off); 
+        string BPawns   = Convert.ToString((long)pieces[Board.Black + Board.Pawns - 1],   toBase: 2).PadLeft(bits, off);
+                                                                                                              
+        string WKnight  = Convert.ToString((long)pieces[Board.White + Board.Knights - 1], toBase: 2).PadLeft(bits, off);
+        string BKnight  = Convert.ToString((long)pieces[Board.Black + Board.Knights - 1], toBase: 2).PadLeft(bits, off);
+                                                                                                              
+        string WBishops = Convert.ToString((long)pieces[Board.White + Board.Bishops - 1], toBase: 2).PadLeft(bits, off);
+        string BBishops = Convert.ToString((long)pieces[Board.Black + Board.Bishops - 1], toBase: 2).PadLeft(bits, off);
+                                                                                                               
+        string WRooks   = Convert.ToString((long)pieces[Board.White + Board.Rooks - 1],   toBase: 2).PadLeft(bits, off);
+        string BRooks   = Convert.ToString((long)pieces[Board.Black + Board.Rooks - 1],   toBase: 2).PadLeft(bits, off);
+                                                                                                               
+        string WQueen   = Convert.ToString((long)pieces[Board.White + Board.Queen - 1],   toBase: 2).PadLeft(bits, off);
+        string BQueen   = Convert.ToString((long)pieces[Board.Black + Board.Queen - 1],   toBase: 2).PadLeft(bits, off);
+                                                                                                               
+        string WKing    = Convert.ToString((long)pieces[Board.White + Board.King - 1],    toBase: 2).PadLeft(bits, off);
+        string BKing    = Convert.ToString((long)pieces[Board.Black + Board.King - 1],    toBase: 2).PadLeft(bits, off);
 
 
         // Display the pieces depending on the bits
         for (int i = 0; i < 64; i++)
         {
-            if (WPawns[i] == '1')
-                Raylib.DrawTextureEx(textures[0], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
-            else if (BPawns[i] == '1')
-                Raylib.DrawTextureEx(textures[1], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
+            //Repeated variables
+            int div = i / 8;
+            int mod = i % 8;
 
-            else if (WKnight[i] == '1')
-                Raylib.DrawTextureEx(textures[2], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
-            else if (BKnight[i] == '1')
-                Raylib.DrawTextureEx(textures[3], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
+            float rotation = 0f;
+            float scale = 0.3f;
 
-            else if (WBishops[i] == '1')
-                Raylib.DrawTextureEx(textures[4], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
-            else if (BBishops[i] == '1')
-                Raylib.DrawTextureEx(textures[5], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
+            Color color = Color.WHITE;
 
-            else if (WRooks[i] == '1')
-                Raylib.DrawTextureEx(textures[6], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
-            else if (BRooks[i] == '1')
-                Raylib.DrawTextureEx(textures[7], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
+            //Pawns
+            if (WPawns[i] == on)
+                Raylib.DrawTextureEx(textures[0], new(starting_pos_x + mod * width - (17 * ratio), 
+                    starting_pos_y + div * width + (10 * ratio)), rotation, scale * ratio, color);
+            else if (BPawns[i] == on)
+                Raylib.DrawTextureEx(textures[1], new(starting_pos_x + mod * width - (17 * ratio), 
+                    starting_pos_y + div * width), rotation, scale * ratio, color);
 
-            else if (WQueen[i] == '1')
-                Raylib.DrawTextureEx(textures[8], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
-            else if (BQueen[i] == '1')
-                Raylib.DrawTextureEx(textures[9], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
+            //Knights
+            else if (WKnight[i] == on)
+                Raylib.DrawTextureEx(textures[2], new(starting_pos_x + mod * width - (10 * ratio), 
+                    starting_pos_y + div * width + (10 * ratio)), rotation, scale * ratio, color);
+            else if (BKnight[i] == on)
+                Raylib.DrawTextureEx(textures[3], new(starting_pos_x + mod * width - (10 * ratio), 
+                    starting_pos_y + div * width), rotation, scale * ratio, color);
 
-            else if (WKing[i] == '1')
-                Raylib.DrawTextureEx(textures[10], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
-            else if (BKing[i] == '1')
-                Raylib.DrawTextureEx(textures[11], new(25 + (i % 8) * 100, 25 + (i / 8) * 100), 0f, 0.3f, Color.WHITE);
+            //Bishops
+            else if (WBishops[i] == on)
+                Raylib.DrawTextureEx(textures[4], new(starting_pos_x + mod * width, 
+                    starting_pos_y + div * width + (10 * ratio)), rotation, scale * ratio, color);
+            else if (BBishops[i] == on)
+                Raylib.DrawTextureEx(textures[5], new(starting_pos_x + mod * width, 
+                    starting_pos_y + div * width), rotation, scale * ratio, color);
+
+            //Rooks
+            else if (WRooks[i] == on)
+                Raylib.DrawTextureEx(textures[6], new(starting_pos_x + mod * width - (10 * ratio), 
+                    starting_pos_y + div * width + (10 * ratio)), rotation, scale * ratio, color);
+            else if (BRooks[i] == on)
+                Raylib.DrawTextureEx(textures[7], new(starting_pos_x + mod * width - (10 * ratio), 
+                    starting_pos_y + div * width), rotation, scale * ratio, color);
+
+            //Queens
+            else if (WQueen[i] == on)
+                Raylib.DrawTextureEx(textures[8], new(starting_pos_x + mod * width + (6 * ratio), 
+                    starting_pos_y + div * width + (10 * ratio)), rotation, scale * ratio, color);
+            else if (BQueen[i] == on)
+                Raylib.DrawTextureEx(textures[9], new(starting_pos_x + mod * width + (6 * ratio), 
+                    starting_pos_y + div * width), rotation, scale * ratio, color);
+
+            //Kings
+            else if (WKing[i] == on)
+                Raylib.DrawTextureEx(textures[10], new(starting_pos_x + mod * width + (10 * ratio), 
+                    starting_pos_y + div * width + (10 * ratio)), rotation, scale * ratio, color);
+            else if (BKing[i] == on)
+                Raylib.DrawTextureEx(textures[11], new(starting_pos_x + mod * width + (10 * ratio), 
+                    starting_pos_y + div * width), rotation, scale * ratio, color);
         }
 
     }
