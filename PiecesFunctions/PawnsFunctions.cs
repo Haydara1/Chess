09dfb1756e9c28856818a1a6f05ed468a758d6f6
Haydara <haydaraal2005@gmail.com>
2023-11-22 13:50:20 +0100

    Added a function for pawns' movements

---
+++ PiecesFunctions/PawnsFunctions.cs
@@ -0,0 +1,16 @@
+﻿namespace Chess;
+
+using static SetwiseFunctions;
+
+internal class PawnsFunctions
+{
+    // Push one square for white pawns
+    static public UInt64 wSinglePush(UInt64 pawns)
+        => NorthOne(pawns) & empty;
+
+    // Push two squares for white pawns
+    static public UInt64 wDoublePush(UInt64 pawns)
+    {
+        return 0;
+    }
+}
