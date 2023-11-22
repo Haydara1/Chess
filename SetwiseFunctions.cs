70180daa2fd4185a8508adf6082dcde5e3efffe5
Haydara <haydaraal2005@gmail.com>
2023-11-22 13:51:13 +0100

    Added class to contain setwise functions

---
+++ SetwiseFunctions.cs
@@ -0,0 +1,15 @@
+﻿namespace Chess;
+
+internal static class SetwiseFunctions
+{
+    // Constants:
+    public static UInt64 empty = 0;
+
+    // Shifts the bits one position to the north.
+    public static UInt64 NorthOne(UInt64 x)
+        => x << 8;
+
+    // Shifts the bits one position to the south.
+    public static UInt64 SouthOne(UInt64 x) 
+        => x >> 8;
+}
