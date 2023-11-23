9d623e4a1ccf95d0b51ff56bb8972008016d2c25
Haydara <haydaraal2005@gmail.com>
2023-11-23 08:35:23 +0100

    Functions for the pawns' push

--- PiecesFunctions/PawnsFunctions.cs
+++ PiecesFunctions/PawnsFunctions.cs
@@ -4,13 +4,31 @@ using static SetwiseFunctions;
 
 internal class PawnsFunctions
 {
+    #region Pawns pushes:
+
+    #region White:
     // Push one square for white pawns
     static public UInt64 wSinglePush(UInt64 pawns)
-        => NorthOne(pawns) & empty;
+        => NorthOne(pawns) & GetEmptySquares(); 
+    //Gets the square in the north of pawns, intersected with empty squares.
 
     // Push two squares for white pawns
     static public UInt64 wDoublePush(UInt64 pawns)
-    {
-        return 0;
-    }
+        => NorthOne(NorthOne(pawns)) & GetEmptySquares() & rank4;
+    //Gets the square in the north of pawns, intersected with empty squares in the fourth rank.
+
+    #endregion
+
+    #region Black:
+    // Push one square for black pawns
+    static public UInt64 bSinglePush(UInt64 pawns)
+        => SouthOne(pawns) & GetEmptySquares();
+
+    // Push two squares for black pawns
+    static public UInt64 bDoublePush(UInt64 pawns)
+        => SouthOne(SouthOne(pawns)) & GetEmptySquares() & rank5;
+
+    #endregion
+
+    #endregion
 }
