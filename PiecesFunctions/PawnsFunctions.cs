d2532aa02b06090f36758a860affac5dc6350a98
Haydara <haydaraal2005@gmail.com>
2023-11-25 15:40:05 +0100

    Added functions to return the possible movements of all the pawns from the same color.

--- PiecesFunctions/PawnsFunctions.cs
+++ PiecesFunctions/PawnsFunctions.cs
@@ -4,9 +4,9 @@ using static SetwiseFunctions;
 
 internal class PawnsFunctions
 {
-    #region Pawns pushes:
+    #region Pawns pushes
 
-    #region White:
+    #region White
     // Push one square for white pawns
     static public UInt64 wSinglePush(UInt64 pawns)
         => NorthOne(pawns) & GetEmptySquares(); 
@@ -19,7 +19,7 @@ internal class PawnsFunctions
 
     #endregion
 
-    #region Black:
+    #region Black
     // Push one square for black pawns
     static public UInt64 bSinglePush(UInt64 pawns)
         => SouthOne(pawns) & GetEmptySquares();
@@ -29,6 +29,47 @@ internal class PawnsFunctions
         => SouthOne(SouthOne(pawns)) & GetEmptySquares() & rank5;
 
     #endregion
+
+    #endregion
+
+    #region Pawns able to push
+
+    #region White
+
+    // Gets the white pawns that are able to push
+    static public UInt64 wAbleToPush(UInt64 pawns, UInt64 empty)
+        => SouthOne(empty) & pawns;
+    
+
+     // Gets the white pawns that are able to double push
+    static public UInt64 wAbleToDoublePush(UInt64 pawns, UInt64 empty)
+        => wAbleToPush(pawns, SouthOne(empty & rank4) & empty);
+
+    #endregion
+
+    #region Black
+
+    // Gets the black pawns that are able to push
+    static public UInt64 bAbleToPush(UInt64 pawns, UInt64 empty)
+        => NorthOne(empty) & pawns;
+
+    // Gets the black pawns that are able to double push
+    static public UInt64 bAbleToDoublePush(UInt64 pawns, UInt64 empty)
+        => bAbleToPush(pawns, NorthOne(empty & rank5) & empty);
+
+    #endregion
+
+    #endregion
+
+    #region Clean results
+
+    // Gets all possible movements for all white pawns
+    static public UInt64 GetWPawnsMovement(UInt64 wpawns)
+        => wSinglePush(wAbleToPush(wpawns, GetEmptySquares())) | wDoublePush(wAbleToDoublePush(wpawns, GetEmptySquares()));
+
+    // Gets all possible movements for all white pawns
+    static public UInt64 GetBPawnsMovement(UInt64 bpawns)
+        => bSinglePush(bAbleToPush(bpawns, GetEmptySquares())) | bDoublePush(bAbleToDoublePush(bpawns, GetEmptySquares()));
 
     #endregion
 }
