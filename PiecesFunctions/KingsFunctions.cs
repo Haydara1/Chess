ee840b18e23df0735568de377552a2e1b09367ca
Haydara <haydaraal2005@gmail.com>
2023-12-01 14:56:34 +0100

    Added possible king movements

---
+++ PiecesFunctions/KingsFunctions.cs
@@ -0,0 +1,15 @@
+﻿namespace Chess;
+
+using static SetwiseFunctions;
+
+internal class KingsFunctions
+{
+    // Returns the possible movements of a king
+    public static UInt64 GetKingMovements(UInt64 king)
+    {
+        UInt64 attacks = EastOne(king) | WestOne(king);
+        king |= attacks;
+        attacks |= NorthOne(king) | SouthOne(king);
+        return attacks & GetEmptySquares();
+    }
+}
