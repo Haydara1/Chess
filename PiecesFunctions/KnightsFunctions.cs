05f247d22b9b39c0cf5e4f3096d745430b1dee3a
Haydara <haydaraal2005@gmail.com>
2023-12-01 10:16:03 +0100

    Added a class for knight related functions

---
+++ PiecesFunctions/KnightsFunctions.cs
@@ -0,0 +1,40 @@
+﻿namespace Chess;
+
+using static SetwiseFunctions;
+
+internal class KnightsFunctions
+{
+    // Knight movements
+    private static UInt64 NoNoEa(UInt64 knight)
+        => (knight << 0) & (0);
+
+    private static UInt64 NoNoWe(UInt64 knight)
+        => (knight << 0) & (0);
+
+    private static UInt64 SoSoEa(UInt64 knight)
+        => (knight << 0) & (0);
+
+    private static UInt64 SoSoWe(UInt64 knight)
+        => (knight << 0) & (0);
+
+    private static UInt64 EaEaNo(UInt64 knight)
+        => (knight << 0) & (0);
+
+    private static UInt64 EaEaSo(UInt64 knight)
+        => (knight << 0) & (0);
+
+    private static UInt64 WeWeNo(UInt64 knight)
+        => (knight << 0) & (0);
+
+    private static UInt64 WeWeSo(UInt64 knight)
+        => (knight << 0) & (0);
+
+    // Group them all in this function
+    public static UInt64 GetKnightMovements(UInt64 knight)
+        => (NoNoEa(knight) | NoNoWe(knight) |
+            SoSoEa(knight) | SoSoWe(knight) |
+            EaEaNo(knight) | EaEaSo(knight) |
+            WeWeNo(knight) | WeWeSo(knight) )
+        & GetEmptySquares();
+    
+}
