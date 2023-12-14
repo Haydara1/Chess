0f4854067d7527322ce63795248a6b169ae2f24f
Haydara <haydaraal2005@gmail.com>
2023-12-14 19:12:54 +0100

    Wrote the functions to the pawns eating

--- PiecesFunctions/PawnsFunctions.cs
+++ PiecesFunctions/PawnsFunctions.cs
@@ -19,6 +19,17 @@ internal class PawnsFunctions
         => NorthOne(NorthOne(pawns)) & GetEmptySquares() & rank['5'];
     //Gets the square in the north of pawns, intersected with empty squares in the fourth rank.
 
+    // Returns one only if there is a black piece on the north west of the pawn
+    static public UInt64 wLeftEat(UInt64 pawns)
+        => NorthOne(WestOne(pawns)) & Board.BlackPieces;
+
+    // Returns one only if there is a black piece on the north east of the pawn
+    static public UInt64 wRightEat(UInt64 pawns)
+        => NorthOne(EastOne(pawns)) & Board.BlackPieces;
+
+    static public UInt64 wEat(UInt64 pawns)
+        => wLeftEat(pawns) | wRightEat(pawns);
+
     #endregion
 
     #region Black
@@ -29,6 +40,17 @@ internal class PawnsFunctions
     // Push two squares for black pawns
     static public UInt64 bDoublePush(UInt64 pawns)
         => SouthOne(SouthOne(pawns)) & GetEmptySquares() & rank['4'];
+
+    // Returns one only if there is a white piece on the north west of the pawn
+    static public UInt64 bLeftEat(UInt64 pawns)
+        => SouthOne(WestOne(pawns)) & Board.WhitePieces;
+
+    // Returns one only if there is a white piece on the north east of the pawn
+    static public UInt64 bRightEat(UInt64 pawns)
+        => SouthOne(EastOne(pawns)) & Board.WhitePieces;
+
+    static public UInt64 bEat(UInt64 pawns)
+        => bLeftEat(pawns) | bRightEat(pawns);
 
     #endregion
 
@@ -67,19 +89,25 @@ internal class PawnsFunctions
 
     // Gets all possible movements for all white pawns
     static public UInt64 GetWPawnsMovement(UInt64 wpawns)
-        => wSinglePush(wAbleToPush(wpawns, GetEmptySquares())) | wDoublePush(wAbleToDoublePush(wpawns, GetEmptySquares()));
+        => wSinglePush(wAbleToPush(wpawns, GetEmptySquares())) 
+        | wDoublePush(wAbleToDoublePush(wpawns, GetEmptySquares()))
+        | wEat(wpawns);
 
     // Gets all possible movements for the white pawn in the file 'index'
     static public UInt64 GetWPawnsMovement(UInt64 wpawns, char index)
-        => (wSinglePush(wAbleToPush(wpawns, GetEmptySquares())) | wDoublePush(wAbleToDoublePush(wpawns, GetEmptySquares()))) & file[index];
+        => GetWPawnsMovement(wpawns) & file[index] 
+        | wEat(wpawns & file[index]);
 
     // Gets all possible movements for all white pawns
     static public UInt64 GetBPawnsMovement(UInt64 bpawns)
-        => bSinglePush(bAbleToPush(bpawns, GetEmptySquares())) | bDoublePush(bAbleToDoublePush(bpawns, GetEmptySquares()));
+        => bSinglePush(bAbleToPush(bpawns, GetEmptySquares())) 
+        | bDoublePush(bAbleToDoublePush(bpawns, GetEmptySquares()))
+        | bEat(bpawns);
 
     // Gets all possible movements for the black pawn in the file 'index'
     static public UInt64 GetBPawnsMovement(UInt64 bpawns, char index)
-        => (bSinglePush(bAbleToPush(bpawns, GetEmptySquares())) | bDoublePush(bAbleToDoublePush(bpawns, GetEmptySquares()))) & file[index];
+        => GetBPawnsMovement(bpawns) & file[index]
+        | bEat(bpawns & file[index]);
 
     #endregion
 }
