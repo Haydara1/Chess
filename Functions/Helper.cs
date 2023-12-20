7cd96cadc5f6571af6ce614cda6e74d1f065ab11
Haydara <haydaraal2005@gmail.com>
2023-12-20 14:13:29 +0100

    Added a class to help the coding, which is not related to the game itself

---
+++ Functions/Helper.cs
@@ -0,0 +1,10 @@
+﻿namespace Chess.Functions;
+
+internal class Helper
+{
+    static public void Print(UInt64 Position)
+        => Console.WriteLine(Convert.ToString((long)Position, 2).PadLeft(64, '0'));
+
+    static public void Print(string text, UInt64 Position)
+        => Console.WriteLine(text + ": " + Convert.ToString((long)Position).PadLeft(64, '0'));
+}
