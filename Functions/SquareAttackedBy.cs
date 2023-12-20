2974b79fd7cdd593278ba488c3f4c91027b813c0
Haydara <haydaraal2005@gmail.com>
2023-12-20 14:12:41 +0100

    Added function to indicate if a specific square is being attacked

---
+++ Functions/SquareAttackedBy.cs
@@ -0,0 +1,42 @@
+﻿namespace Chess.Functions;
+
+using System.Security.Principal;
+using static Board;
+
+internal class SquareAttackedBy
+{
+    // Returns true if the given square is attacked by a piece from the opposing side
+    static public bool isSquareAttacked(UInt64 sq, short side)
+    {
+        // Take the colour
+        int color = side == 0 ? 0 : 6;
+
+        // Check for knights
+        if ((KnightsFunctions.GetKnightMovements(piecesBB[color + Knights - 1]) & sq) != 0)
+            return true;
+
+        // Check for bishops
+        if ((BishopsFunctions.GetBishopsMovements(piecesBB[color + Bishops - 1]) & sq) != 0)
+            return true;
+
+        // Check for rooks
+        if ((RooksFunctions.GetRookMovements(piecesBB[color + Rooks - 1]) & sq) != 0)
+            return true;
+
+        // Check for queens
+        if ((QueensFunctions.GetQueenMovements(piecesBB[color + Queen - 1]) & sq) != 0)
+            return true;
+
+        // Lastly check for pawns
+        if (side == 0)
+        {
+            if ((PawnsFunctions.wEat(piecesBB[color + Pawns - 1]) & sq) != 0)
+                return true;
+        }
+        else
+            if ((PawnsFunctions.bEat(piecesBB[color + Pawns - 1]) & sq) != 0)
+                return true;
+
+        return false;
+    }
+}
