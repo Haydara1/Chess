ea93c5e71282eb686d2fc6f345bb1e2b1fa03384
Haydara <haydaraal2005@gmail.com>
2023-12-24 13:59:53 +0100

    Added functions to facilate "is square attacked" function

--- PiecesFunctions/KingsFunctions.cs
+++ PiecesFunctions/KingsFunctions.cs
@@ -1,18 +1,33 @@
 ﻿namespace Chess;
 
+using Chess.Functions;
+
 // https://www.chessprogramming.org/King_Pattern
 
 using static SetwiseFunctions;
 
 internal class KingsFunctions
 {
-    // Returns the possible movements of a king
+    // Returns the legal movements of a king
     public static UInt64 GetKingMovements(UInt64 king)
     {
         UInt64 attacks = EastOne(king) | WestOne(king);
         king |= attacks;
         attacks |= NorthOne(king) | SouthOne(king);
+
+        attacks = GeneralFunctions.TurnOffAttackedBits(attacks);
+
         return attacks & (GetEmptySquares() | (Convert.ToBoolean(Program.turn) 
-            ? Board.WhitePieces : Board.BlackPieces));
+            ? Board.WhitePieces : Board.BlackPieces)); 
     }
+
+    // Returns the possible movements of a king
+    public static UInt64 GetAllKingMovements(UInt64 king)
+    {
+        UInt64 attacks = EastOne(king) | WestOne(king);
+        king |= attacks;
+        attacks |= NorthOne(king) | SouthOne(king);
+        return attacks;
+    }
+
 }
