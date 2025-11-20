516f0d1cccd85b5baa4d6b340c6a8e6769f85904
HpG9 <HpG9@DESKTOP-BEL4I0K>
2025-11-20 21:29:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Raylib_cs;
+
+namespace Chess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 720;
+            int height = 720;
+
+            Raylib.InitWindow(width, height, "Chess");
+
+            // Game loop
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+
+                bool color = false;
+
+                // Draw chess board
+                for (int i = 0; i < 8; i++)
+                {
+                    color = !color;
+                    for (int j = 0; j < 8; j++)
+                    {
+                        Raylib.DrawRectangle(i * 40, j * 40, 40, 40, 
+                            color ? Color.Black : Color.Red);
+                        color = !color;
+                    }
+                }
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
