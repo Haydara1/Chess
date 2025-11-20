b6af71ff1b08fad430832f9542a09fb0249f187e
Haydara <haydaraal2005@gmail.com>
2025-11-20 22:28:53 +0100

    Added Back end function to contain board, pieces, and related info.

---
+++ Backend.cs
@@ -0,0 +1,25 @@
+﻿namespace Chess;
+
+public static class Piece
+{
+    public const int None = 0;
+    public const int King = 1;
+    public const int Queen = 2;
+    public const int Bishop = 3;
+    public const int Knight = 4;
+    public const int Rook = 5;
+    public const int Pawn = 6;
+
+    public const int White = 8;
+    public const int Black = 16;
+}
+
+public class Board
+{
+    public int[] Square;
+
+    public Board()
+    {
+        Square = new int[64];
+    }
+}
